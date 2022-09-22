dd2d4159d50c108e8fe9ddfac77688e1de96b443
Vadim Ermolin <vadimyer@gmail.com>
2022-09-22 15:42:28 +0200

    Rewrite bits of Vimscript in Lua

---
+++ core.lua
@@ -0,0 +1,55 @@
+-- Encoding.
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.opt.fileencodings = 'utf-8'
+
+-- Fix backspace indent.
+vim.opt.backspace = 'indent,eol,start'
+
+-- Tabs.
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 0
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Map leader to ,
+vim.g.mapleader = ','
+
+-- Enable hidden buffers.
+vim.opt.hidden = true
+
+-- Search functionality.
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Misc.
+vim.opt.fileformats = 'unix,dos,mac'
+vim.opt.clipboard = 'unnamedplus'
+vim.nocompatible = true
+vim.nowrap = true
+vim.nobackup = true
+vim.nowritebackup = true
+vim.opt.showtabline = 999
+vim.opt.nu = true
+vim.opt.ruler = true
+vim.opt.updatetime = 100
+vim.opt.cc = '80'
+vim.opt.signcolumn = 'yes'
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
+-- Disable netrw.
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Shell preference.
+if vim.fn.exists('$SHELL') then
+  vim.opt.shell = '$SHELL'
+else
+  vim.opt.shell = '/bin/sh'
+end
+
+-- Blinking cursor.
+vim.opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250,sm:block-blinkwait175-blinkoff150-blinkon175'
--- plug.lua
+++ plug.lua
@@ -1,16 +1,18 @@
-call plug#begin()
-" Mason and stuff.
+local Plug = vim.fn['plug#']
+
+vim.fn.has('plug#begin')
+-- Mason and stuff.
 Plug 'williamboman/mason.nvim'
 Plug 'williamboman/mason-lspconfig.nvim'
 Plug 'WhoIsSethDaniel/mason-tool-installer.nvim'
 Plug 'neovim/nvim-lspconfig'
 
-" Navigator with signature plugin.
-Plug 'ray-x/guihua.lua', {'do': 'cd lua/fzy && make' }
+-- Navigator with signature plugin.
+Plug('ray-x/guihua.lua', {['do'] = 'cd lua/fzy && make' })
 Plug 'ray-x/navigator.lua'
 Plug 'ray-x/lsp_signature.nvim'
 
-" nvim-cmp and stuff.
+-- nvim-cmp and stuff.
 Plug 'onsails/lspkind-nvim'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
@@ -20,35 +22,35 @@ Plug 'hrsh7th/nvim-cmp'
 Plug 'saadparwaiz1/cmp_luasnip'
 Plug 'windwp/nvim-autopairs'
 
-" Highlighter.
+-- Highlighter.
 Plug 'rrethy/vim-illuminate'
 
-" Snippets.
-Plug 'L3MON4D3/LuaSnip', {'tag': 'v<CurrentMajor>.*'}
+-- Snippets.
+Plug('L3MON4D3/LuaSnip', {['tag'] = 'v<CurrentMajor>.*'})
 Plug 'rafamadriz/friendly-snippets'
 
-" NERDTree.
-Plug 'kyazdani42/nvim-web-devicons' " optional, for file icons
+-- NvimTree.
+Plug 'kyazdani42/nvim-web-devicons'
 Plug 'kyazdani42/nvim-tree.lua'
 
-" Lualine.
+-- Lualine.
 Plug 'nvim-lualine/lualine.nvim'
 
-" Git.
+-- Git.
 Plug 'lewis6991/gitsigns.nvim'
 
-" Indentation guides.
+-- Indentation guides.
 Plug 'lukas-reineke/indent-blankline.nvim'
 
-" Treesitter.
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+-- Treesitter.
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug 'tree-sitter/tree-sitter-go'
 
-" Themes, schemes and icons.
-Plug 'folke/tokyonight.nvim', { 'branch': 'main' }
+-- Themes, schemes and icons.
+Plug('folke/tokyonight.nvim', {['branch'] = 'main' })
 Plug 'sainnhe/everforest'
 Plug 'cormacrelf/vim-colors-github'
 Plug 'ryanoasis/vim-devicons'
-call plug#end()
+vim.fn.has('plug#end')
 
-""" TODO: """ Highlight whitespace and add final newline char.
+-- TODO: """ Highlight whitespace and add final newline char.
