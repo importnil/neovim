0a5164147665e7f8649a7488a66420c0c828408d
Vadim Ermolin <vadimyer@gmail.com>
2022-09-29 22:01:45 +0200

    Startup without rewind after PlugInstall

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd('VimEnter', {
-  command = [[if len(filter(values(g:plugs), '!isdirectory(v:val.dir)')) |  PlugInstall --sync | q | rewind |endif]]
-})
+vim.api.nvim_create_autocmd({
+    command = [[if len(filter(values(g:plugs), '!isdirectory(v:val.dir)')) |  PlugInstall --sync | q |endif]]
+}, 'VimEnter')
 
 -- Entrypoint for all plugins and configurations.
 require('core')
