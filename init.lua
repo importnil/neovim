3fbee436b65c17aa478355b21e4fcd1c1cc1b267
Vadim Ermolin <vadimyer@gmail.com>
2023-01-23 12:51:36 +0100

    main: remove VimEnter autocmd

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_create_autocmd('VimEnter', {
-    command = [[if len(filter(values(g:plugs), '!isdirectory(v:val.dir)')) | PlugInstall --sync | q | rewind | endif]]
-})
-
 -- Entrypoint for all plugins and configurations.
 require('core')
 require('plug')
