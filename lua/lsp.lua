bfb15edf43a3dd7e42b747a5881a064babe83e23
Vadim Ermolin <vadimyer@gmail.com>
2023-01-30 15:59:37 +0100

    main: format some vim code

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,10 +1,10 @@
 -- LSP finalizers.
 vim.cmd [[
-  augroup lsp_finalizers
-    autocmd!
-    autocmd InsertLeave,BufWritePre *.go,*.rs,*.lua :lua org_imports(3000)
-    autocmd InsertLeave,BufWritePre *.go,*.rs,*.lua :lua vim.lsp.buf.format()
-  augroup END
+    augroup lsp_finalizers
+        autocmd!
+        autocmd InsertLeave,BufWritePre *.go,*.rs,*.lua :lua org_imports(3000)
+        autocmd InsertLeave,BufWritePre *.go,*.rs,*.lua :lua vim.lsp.buf.format()
+    augroup END
 ]]
 
 -- General LSP configuration.
